734c5f695396e8063155a0c1c3779c3613dfedcc
luchanso <oleg@loutchansky.com>
2016-05-23 10:48:27 +0300

    split project

---
+++ public/js/game/square.js
@@ -0,0 +1,23 @@
+var Square = function(game) {
+  const defaultPlayerColor = 0xEEFF41,
+    defaultSquareMoveTime = 75;
+
+  this.game = game;
+  this.playerColor = defaultPlayerColor;
+  this.squareMoveTime = defaultSquareMoveTime;
+
+}
+
+Square.prototype = {
+  preload: function() {
+
+  },
+
+  create: function() {
+
+  },
+
+  update: function() {
+    
+  }
+};
