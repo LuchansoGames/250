2945af835e97afc259d437d5852bdb52800f828c
luchanso <oleg@loutchansky.com>
2016-06-19 14:05:48 +0300

    Auto Update

--- app.js
+++ app.js
@@ -75,9 +75,7 @@ setTimeout(function() {
   function vkAdsInit() {
     ADSOnLoad(onLoad);
     VK.Widgets.Ads('vk_ads_75686', {}, adsParams);
-    VK.init({
-      apiId:5448474
-    });
+    
     console.log('test');
     var user_id = 161236502;   //id пользователя 
     var app_id = 5448474;  //id вашего приложения
