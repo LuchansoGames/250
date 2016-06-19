4cc37c543148c641539b94e6c295de0c02b57d8a
luchanso <oleg@loutchansky.com>
2016-06-19 14:38:23 +0300

    Auto Update

--- app.js
+++ app.js
@@ -75,28 +75,22 @@ setTimeout(function() {
   function vkAdsInit() {
     ADSOnLoad(onLoad);
     VK.Widgets.Ads('vk_ads_75686', {}, adsParams);
-    VK.init({
-      apiId: 5448474
-    });
+    console.log('test 2');
+    var user_id = null;   //id пользователя 
+    var app_id = 5448474;  //id вашего приложения
+    var a = new VKAdman();
+    a.onNoAds(function(){console.log("Adman: No ads");});
+    a.onStarted(function(){console.log("Adman: Started");});
+    a.onCompleted(function(){console.log("Adman: Completed");});
+    a.onSkipped(function(){console.log("Adman: Skipped");});
+    a.onClicked(function(){console.log("Adman: Clicked");});
 
-    setTimeout(function() {
-      console.log('test 2');
-      var user_id = null;   //id пользователя 
-      var app_id = 5448474;  //id вашего приложения
-      var a = new VKAdman();
-      a.onNoAds(function(){console.log("Adman: No ads");});
-      a.onStarted(function(){console.log("Adman: Started");});
-      a.onCompleted(function(){console.log("Adman: Completed");});
-      a.onSkipped(function(){console.log("Adman: Skipped");});
-      a.onClicked(function(){console.log("Adman: Clicked");});
+    // Для проверки корректности работы рекламы
+    // a.setupPreroll(app_id, {preview: 8});
+    a.setupPreroll(app_id);
 
-      // Для проверки корректности работы рекламы
-      // a.setupPreroll(app_id, {preview: 8});
-      a.setupPreroll(app_id);
-
-      admanStat(app_id, user_id);
-      console.log('test');
-    }, 4000);
+    admanStat(app_id, user_id);
+    console.log('test');
   }
   if (window.VK && VK.Widgets) {
     vkAdsInit();
